5cd7cd70aff7eb0bf5940a5e48af656c1e832854
Kevin Durbin <kevindurb@fastmail.com>
2023-12-27 14:10:23 -0700

    refactoring formatter config, add pug

--- dot_config/nvim/lua/plugins/formatter.lua
+++ dot_config/nvim/lua/plugins/formatter.lua
@@ -1,43 +1,49 @@
 return {
-  'mhartington/formatter.nvim', -- auto format files
-  opts = function()
-    return {
-      filetype = {
-        typescript = {
-          require('formatter.filetypes.typescript').biome,
-          require('formatter.filetypes.typescript').prettierd,
-          require('formatter.filetypes.typescript').eslint_d,
-        },
-        javascript = {
-          require('formatter.filetypes.javascript').biome,
-          require('formatter.filetypes.javascript').prettierd,
-          require('formatter.filetypes.javascript').eslint_d,
-        },
-        typescriptreact = {
-          require('formatter.filetypes.typescript').biome,
-          require('formatter.filetypes.typescript').prettierd,
-          require('formatter.filetypes.typescript').eslint_d,
-        },
-        javascriptreact = {
-          require('formatter.filetypes.javascript').biome,
-          require('formatter.filetypes.javascript').prettierd,
-          require('formatter.filetypes.javascript').eslint_d,
-        },
-        css = { require('formatter.filetypes.css').prettierd },
-        graphql = { require('formatter.filetypes.graphql').prettierd },
-        html = { require('formatter.filetypes.html').prettierd },
-        json = { require('formatter.filetypes.json').prettierd },
-        lua = { require('formatter.filetypes.lua').stylua },
-        markdown = { require('formatter.filetypes.markdown').prettierd },
-        php = { require('formatter.filetypes.php').php_cs_fixer },
-        yaml = { require('formatter.filetypes.yaml').prettierd },
-      },
-      ['*'] = { require('formatter.filetypes.any').remove_trailing_whitespace },
-    }
-  end,
-  init = function()
-    vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
-      command = 'FormatWriteLock',
-    })
-  end,
+	"mhartington/formatter.nvim", -- auto format files
+	opts = function()
+    local util = require("formatter.util")
+    local defaults = require("formatter.defaults")
+    local typescript = require("formatter.filetypes.typescript")
+    local javascript = require("formatter.filetypes.javascript")
+
+		return {
+			filetype = {
+				typescript = {
+					typescript.biome,
+					typescript.prettierd,
+					typescript.eslint_d,
+				},
+				javascript = {
+					javascript.biome,
+					javascript.prettierd,
+					javascript.eslint_d,
+				},
+				typescriptreact = {
+					typescript.biome,
+					typescript.prettierd,
+					typescript.eslint_d,
+				},
+				javascriptreact = {
+					javascript.biome,
+					javascript.prettierd,
+					javascript.eslint_d,
+				},
+				css = { require("formatter.filetypes.css").prettierd },
+				graphql = { require("formatter.filetypes.graphql").prettierd },
+				html = { require("formatter.filetypes.html").prettierd },
+				json = { require("formatter.filetypes.json").prettierd },
+				lua = { require("formatter.filetypes.lua").stylua },
+				markdown = { require("formatter.filetypes.markdown").prettierd },
+				php = { require("formatter.filetypes.php").php_cs_fixer },
+				yaml = { require("formatter.filetypes.yaml").prettierd },
+				pug = { util.copyf(defaults.prettierd) },
+			},
+			["*"] = { require("formatter.filetypes.any").remove_trailing_whitespace },
+		}
+	end,
+	init = function()
+		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+			command = "FormatWriteLock",
+		})
+	end,
 }
