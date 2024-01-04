4c4d02b23afb42a421b667c1341f2aafe1299078
Kevin Durbin <kevindurb@fastmail.com>
2024-01-04 13:54:39 -0700

    Update .config/nvim/lua/plugins/init.lua

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,15 +1,16 @@
 -- Just basic plugins that dont need
 -- any setup
 return {
-  'tpope/vim-fugitive', -- git commands in vim
-  'tpope/vim-repeat', -- repeating plugin commands
-  'tpope/vim-surround', -- surround with things
-  'tpope/vim-unimpaired', -- mappings for things with '['
-  'tpope/vim-eunuch', -- helpful unixy commands
-  'tpope/vim-abolish', -- messing with words and casing
-  'godlygeek/tabular', -- auto alignment
-  { 'NoahTheDuke/vim-just', ft = 'just' }, -- justfile
-  { 'Glench/Vim-Jinja2-Syntax', ft = 'jinja2' }, -- jinja2 syntax
-  'nvim-tree/nvim-web-devicons', -- cool icons
-  'lukas-reineke/indent-blankline.nvim', -- indent guides
+	"tpope/vim-fugitive", -- git commands in vim
+	"tpope/vim-repeat", -- repeating plugin commands
+	"tpope/vim-surround", -- surround with things
+	"tpope/vim-unimpaired", -- mappings for things with '['
+	"tpope/vim-eunuch", -- helpful unixy commands
+	"tpope/vim-abolish", -- messing with words and casing
+	"godlygeek/tabular", -- auto alignment
+	{ "NoahTheDuke/vim-just", ft = "just" }, -- justfile
+	{ "Glench/Vim-Jinja2-Syntax", ft = "jinja2" }, -- jinja2 syntax
+	"isobit/vim-caddyfile", -- caddyfile highlighting
+	"nvim-tree/nvim-web-devicons", -- cool icons
+	"lukas-reineke/indent-blankline.nvim", -- indent guides
 }
