c7e455b51b172e100efd2c21ea2f3b1da80fb192
Kevin Durbin <kevindurb@fastmail.com>
2024-01-12 08:11:50 -0700

    Update .config/nvim/lua/kevindurb/colorscheme.lua Remove .config/nvim/lua/plugins/onedark.lua Add .config/nvim/lua/plugins/tokyonight.lua

--- dot_config/nvim/lua/kevindurb/colorscheme.lua
+++ dot_config/nvim/lua/kevindurb/colorscheme.lua
@@ -2,11 +2,11 @@ local M = {}
 
 -- asdfasdfasdfasd
 M.setup = function()
-  vim.cmd([[
-    set t_ZH=^[[3m
-    set t_ZR=^[[23m
-    highlight Comment cterm=italic gui=italic
-  ]])
+  -- vim.cmd([[
+  --   set t_ZH=^[[3m
+  --   set t_ZR=^[[23m
+  --   highlight Comment cterm=italic gui=italic
+  -- ]])
 end
 
 return M
--- dot_config/nvim/lua/plugins/onedark.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'joshdick/onedark.vim',
-  name = 'onedark',
-  lazy = true,
-  init = function()
-    vim.o.background = 'dark'
-    vim.cmd.colorscheme('onedark')
-  end,
-}
