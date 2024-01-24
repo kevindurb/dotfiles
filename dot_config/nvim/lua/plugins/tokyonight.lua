306db227c3810d310ce561347e5818d95c40dd25
Kevin Durbin <kevindurb@fastmail.com>
2024-01-24 13:12:24 -0700

    Update .config/nvim/lua/plugins/tokyonight.lua

--- dot_config/nvim/lua/plugins/tokyonight.lua
+++ dot_config/nvim/lua/plugins/tokyonight.lua
@@ -1,12 +1,12 @@
 return {
-  'folke/tokyonight.nvim',
-  lazy = false,
-  priority = 1000,
-  opts = {
-    style = 'storm',
-  },
-  init = function()
-    vim.o.background = 'dark'
-    vim.cmd.colorscheme('tokyonight')
-  end,
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		style = "storm",
+	},
+	init = function()
+		vim.o.background = "dark"
+		vim.cmd.colorscheme("tokyonight")
+	end,
 }
