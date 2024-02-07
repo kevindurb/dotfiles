ccef8bff2cbfc61e34ba2ffc3e4929bd0beab92c
Kevin Durbin <kevindurb@fastmail.com>
2024-02-07 08:13:01 -0700

    switch vim to catppuccin

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -1,6 +1,7 @@
 return {
-  'shaunsingh/nord.nvim',
-  enabled = false,
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
   init = function()
     vim.o.background = 'dark'
     vim.cmd.colorscheme('nord')
