6b21c1b418dd5987a17afd98bf4d8434c893131e
Kevin Durbin <kevindurb@fastmail.com>
2024-02-07 07:48:08 -0700

    Update .config/nvim/lua/plugins/nvim-lspconfig.lua

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -25,5 +25,7 @@ return {
       local hl = 'DiagnosticSign' .. type
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
     end
+
+    require('lspconfig').snyk.setup()
   end,
 }
