466a900d930e55872d70415660bcbede210c3525
Kevin Durbin <kevindurb@fastmail.com>
2024-02-07 08:15:26 -0700

    fix catppuccin theme

--- dot_config/nvim/lua/plugins/catppuccin.lua
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -2,8 +2,15 @@ return {
   'catppuccin/nvim',
   name = 'catppuccin',
   priority = 1000,
+  opts = {
+    flavor = 'macchiato',
+    background = {
+      light = 'latte',
+      dark = 'macchiato',
+    },
+  },
   init = function()
     vim.o.background = 'dark'
-    vim.cmd.colorscheme('nord')
+    vim.cmd.colorscheme('catppuccin')
   end,
 }
