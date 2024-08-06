388b2a955313171ee4e70d5c085bbdd80437e663
Kevin Durbin <kevindurb@fastmail.com>
2024-08-06 11:11:54 -0600

    clean up deps

--- dot_config/nvim/lua/plugins/mason-lspconfig.lua
+++ dot_config/nvim/lua/plugins/mason-lspconfig.lua
@@ -1,7 +1,6 @@
 return {
   'williamboman/mason-lspconfig.nvim', -- connects mason to lspconfig
   dependencies = {
-    'b0o/schemastore.nvim',
     'williamboman/mason.nvim',
   },
   config = function() end,
