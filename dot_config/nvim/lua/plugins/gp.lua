f148f3888da1c5ad6be2471a9750bf3c5731fdef
Kevin Durbin <kevindurb@fastmail.com>
2024-09-03 11:26:00 -0600

    feat: add gp

--- dot_config/nvim/lua/plugins/gp.lua
+++ dot_config/nvim/lua/plugins/gp.lua
@@ -6,5 +6,20 @@ return {
         endpoint = 'http://localhost:11434/v1/chat/completions',
       },
     },
+    agents = {
+      {
+        provider = 'ollama',
+        name = 'ChatOllamaLlama3.1-8B',
+        chat = true,
+        command = false,
+        model = {
+          model = 'llama3.1',
+          temperature = 0.6,
+          top_p = 1,
+          min_p = 0.05,
+        },
+        system_prompt = 'You are a general AI assistant.',
+      },
+    },
   },
 }
