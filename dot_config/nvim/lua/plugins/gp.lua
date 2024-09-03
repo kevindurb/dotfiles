36b7129f7f6a99ac32e1afdb3d73b59ef70607ff
Kevin Durbin <kevindurb@fastmail.com>
2024-09-03 11:45:52 -0600

    feat: include config for provider and agents

--- dot_config/nvim/lua/plugins/gp.lua
+++ dot_config/nvim/lua/plugins/gp.lua
@@ -2,6 +2,43 @@ return {
   'robitx/gp.nvim',
   opts = {
     default_chat_agent = 'ChatOllamaLlama3.1-8B',
-    default_command_agent = 'CodeOllamaLlama3.1-8B'
+    default_command_agent = 'CodeOllamaLlama3.1-8B',
+    providers = {
+      ollama = {
+        endpoint = 'http://localhost:11434/v1/chat/completions',
+      },
+    },
+    agents = {
+      {
+        provider = 'ollama',
+        name = 'ChatOllamaLlama3.1-8B',
+        chat = true,
+        command = false,
+        -- string with model name or table with model name and parameters
+        model = {
+          model = 'llama3.1',
+          temperature = 0.6,
+          top_p = 1,
+          min_p = 0.05,
+        },
+        -- system prompt (use this to specify the persona/role of the AI)
+        system_prompt = 'You are a general AI assistant.',
+      },
+      {
+        provider = 'ollama',
+        name = 'CodeOllamaLlama3.1-8B',
+        chat = false,
+        command = true,
+        -- string with model name or table with model name and parameters
+        model = {
+          model = 'llama3.1',
+          temperature = 0.4,
+          top_p = 1,
+          min_p = 0.05,
+        },
+        -- system prompt (use this to specify the persona/role of the AI)
+        system_prompt = require('gp.defaults').code_system_prompt,
+      },
+    },
   },
 }
