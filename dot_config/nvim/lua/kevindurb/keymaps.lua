7b544cdf881e61f6eb968ab84a39588287d9e667
Kevin Durbin <kevindurb@fastmail.com>
2024-09-04 07:34:02 -0600

    chore: clean up keymaps

--- dot_config/nvim/lua/kevindurb/keymaps.lua
+++ dot_config/nvim/lua/kevindurb/keymaps.lua
@@ -1,15 +1,21 @@
 local M = {}
 
 M.setup = function()
-  vim.cmd([[
-    " saner searching directions
-    nnoremap <expr> n  'Nn'[v:searchforward]
-    xnoremap <expr> n  'Nn'[v:searchforward]
-    onoremap <expr> n  'Nn'[v:searchforward]
-    nnoremap <expr> N  'nN'[v:searchforward]
-    xnoremap <expr> N  'nN'[v:searchforward]
-    onoremap <expr> N  'nN'[v:searchforward]
-  ]])
+  local map = vim.keymap.set
+
+  -- Resize window using <ctrl> and arrow keys
+  map('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
+  map('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
+  map('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
+  map('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
+
+  -- Saner search directions
+  map('n', 'n', "'Nn'[v:searchforward].'zv'", { expr = true, desc = 'Next Search Result' })
+  map('x', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next Search Result' })
+  map('o', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next Search Result' })
+  map('n', 'N', "'nN'[v:searchforward].'zv'", { expr = true, desc = 'Prev Search Result' })
+  map('x', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev Search Result' })
+  map('o', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev Search Result' })
 end
 
 return M
