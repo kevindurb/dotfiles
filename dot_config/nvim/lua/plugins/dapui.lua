2aadaed9d5636f561db389519a51e370943e64bf
Kevin Durbin <kevindurb@fastmail.com>
2024-09-09 12:08:13 -0600

    feat: add auto install for dap

--- dot_config/nvim/lua/plugins/dapui.lua
+++ dot_config/nvim/lua/plugins/dapui.lua
@@ -1,8 +1,11 @@
 return {
-  'rcarriga/nvim-dap-ui', -- debugging ui
+  'rcarriga/nvim-dap-ui',
   dependencies = {
-    'mfussenegger/nvim-dap', -- debugging
     'nvim-neotest/nvim-nio',
+    {
+      'theHamsta/nvim-dap-virtual-text',
+      opts = {},
+    },
   },
   config = function()
     local dapui = require('dapui')
@@ -22,50 +25,5 @@ return {
     dap.listeners.before.event_exited.dapui_config = function()
       dapui.close()
     end
-
-    vim.keymap.set('n', '<F5>', function()
-      dap.continue()
-    end)
-
-    vim.keymap.set('n', '<F2>', function()
-      dap.step_over()
-    end)
-
-    vim.keymap.set('n', '<F3>', function()
-      dap.step_into()
-    end)
-
-    vim.keymap.set('n', '<F4>', function()
-      dap.step_out()
-    end)
-
-    vim.keymap.set('n', '<F6>', function()
-      dap.close()
-      dapui.close()
-    end)
-
-    vim.keymap.set('n', '<F10>', function()
-      dap.toggle_breakpoint()
-    end)
-
-    vim.keymap.set('n', '<Leader>dr', function()
-      dap.repl.open()
-    end)
-
-    vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
-      widgets.hover()
-    end)
-
-    vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
-      widgets.preview()
-    end)
-
-    vim.keymap.set('n', '<Leader>df', function()
-      widgets.centered_float(widgets.frames)
-    end)
-
-    vim.keymap.set('n', '<Leader>ds', function()
-      widgets.centered_float(widgets.scopes)
-    end)
   end,
 }
---
+++ dot_config/nvim/lua/plugins/mason-nvim-dap.lua
@@ -0,0 +1,15 @@
+return {
+  'jay-babu/mason-nvim-dap.nvim',
+  dependencies = {
+    'williamboman/mason.nvim',
+  },
+  opts = {
+    automatic_installation = true,
+    ensure_installed = {
+      'php',
+      'js',
+      'firefox',
+      'chrome',
+    },
+  },
+}
