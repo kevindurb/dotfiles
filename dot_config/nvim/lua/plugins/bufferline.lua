9f1eeaf995dfadfb6ea641cdba2e9538307930cc
Kevin Durbin <kevindurb@fastmail.com>
2024-09-12 07:57:25 -0600

    fix: underline and hover dont work with alacritty

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -5,14 +5,6 @@ return {
     options = {
       separator_style = 'slant',
       diagnostics = 'nvim_lsp',
-      indicator = {
-        style = 'underline',
-      },
-      hover = {
-        enabled = true,
-        delay = 200,
-        reveal = { 'close' },
-      },
     },
   },
 }
