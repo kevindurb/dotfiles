5187c341193a01e579c33720e13254b1925f0668
Kevin Durbin <kevindurb@fastmail.com>
2024-09-12 07:54:41 -0600

    feat: more pretty things

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -2,6 +2,17 @@
 return {
   'akinsho/bufferline.nvim', -- pretty tabs
   opts = {
-    options = { separator_style = 'slant', diagnostics = 'nvim_lsp' },
+    options = {
+      separator_style = 'slant',
+      diagnostics = 'nvim_lsp',
+      indicator = {
+        style = 'underline',
+      },
+      hover = {
+        enabled = true,
+        delay = 200,
+        reveal = { 'close' },
+      },
+    },
   },
 }
