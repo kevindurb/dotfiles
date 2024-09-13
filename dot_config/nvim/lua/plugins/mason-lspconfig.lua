cf2e605a626a3e96986f31e1571b1d438d21e326
Kevin Durbin <kevindurb@fastmail.com>
2024-09-12 21:16:22 -0600

    Revert "clean up configs"
    This reverts commit 8371b02b8a5abea91cb0e91373c1c7f0bbf6b67a.

--- dot_config/nvim/lua/plugins/mason-lspconfig.lua
+++ dot_config/nvim/lua/plugins/mason-lspconfig.lua
@@ -2,63 +2,6 @@ return {
   'williamboman/mason-lspconfig.nvim', -- connects mason to lspconfig
   dependencies = {
     'williamboman/mason.nvim',
-    'VonHeikemen/lsp-zero.nvim',
   },
-  config = function()
-    local lsp_zero = require('lsp-zero')
-    require('mason-lspconfig').setup({
-      ensure_installed = {
-        'ansiblels',
-        'bashls',
-        'cssls',
-        'dockerls',
-        'eslint',
-        'graphql',
-        'html',
-        'intelephense',
-        'jsonls',
-        'lua_ls',
-        'marksman',
-        'sqlls',
-        'ts_ls',
-        'yamlls',
-      },
-      handlers = {
-        lsp_zero.default_setup,
-
-        lua_ls = function()
-          local lua_opts = lsp_zero.nvim_lua_ls()
-          require('lspconfig').lua_ls.setup(lua_opts)
-        end,
-
-        yamlls = function()
-          local config = {
-            settings = {
-              yaml = {
-                schemaStore = {
-                  enable = false,
-                  url = '',
-                },
-                schemas = require('schemastore').yaml.schemas(),
-              },
-            },
-          }
-
-          config.settings.yaml.schemas['kubernetes'] = 'k8s/**/*.{yml,yaml}'
-          require('lspconfig').yamlls.setup(config)
-        end,
-
-        jsonls = function()
-          require('lspconfig').jsonls.setup({
-            settings = {
-              json = {
-                schemas = require('schemastore').json.schemas(),
-                validate = { enable = true },
-              },
-            },
-          })
-        end,
-      },
-    })
-  end,
+  config = function() end,
 }
--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -9,13 +9,65 @@ return {
   },
   init = function()
     local lsp_zero = require('lsp-zero')
-    local lsp_attach = function(client, bufnr)
+    lsp_zero.extend_lspconfig()
+
+    lsp_zero.on_attach(function(client, bufnr)
       lsp_zero.default_keymaps({ buffer = bufnr })
-    end
-    lsp_zero.extend_lspconfig({
-      sign_text = true,
-      lsp_attach = lsp_attach,
-      capabilities = require('cmp_nvim_lsp').default_capabilities()
+    end)
+
+    require('mason-lspconfig').setup({
+      ensure_installed = {
+        'ansiblels',
+        'bashls',
+        'cssls',
+        'dockerls',
+        'eslint',
+        'graphql',
+        'html',
+        'intelephense',
+        'jsonls',
+        'lua_ls',
+        'marksman',
+        'sqlls',
+        'ts_ls',
+        'yamlls',
+      },
+      handlers = {
+        lsp_zero.default_setup,
+
+        lua_ls = function()
+          local lua_opts = lsp_zero.nvim_lua_ls()
+          require('lspconfig').lua_ls.setup(lua_opts)
+        end,
+
+        yamlls = function()
+          local config = {
+            settings = {
+              yaml = {
+                schemaStore = {
+                  enable = false,
+                  url = '',
+                },
+                schemas = require('schemastore').yaml.schemas(),
+              },
+            },
+          }
+
+          config.settings.yaml.schemas['kubernetes'] = 'k8s/**/*.{yml,yaml}'
+          require('lspconfig').yamlls.setup(config)
+        end,
+
+        jsonls = function()
+          require('lspconfig').jsonls.setup({
+            settings = {
+              json = {
+                schemas = require('schemastore').json.schemas(),
+                validate = { enable = true },
+              },
+            },
+          })
+        end,
+      },
     })
 
     -- Global mappings.
