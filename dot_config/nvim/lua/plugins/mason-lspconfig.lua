0b76df2fd980f713334c8f8a30917f6df5fd9e43
Kevin Durbin <kevindurb@fastmail.com>
2024-09-12 21:40:38 -0600

    lazy load schema store

--- dot_config/nvim/lua/plugins/mason-lspconfig.lua
+++ dot_config/nvim/lua/plugins/mason-lspconfig.lua
@@ -40,16 +40,45 @@ return {
         local capabilities = require('cmp_nvim_lsp').default_capabilities()
         require('lspconfig').yamlls.setup({
           capabilities = capabilities,
-          settings = { yaml = { schemas = require('schemastore').yaml.schemas() } },
+          on_new_config = function(new_config)
+            new_config.settings.yaml.schemas = vim.tbl_deep_extend(
+              "force",
+              new_config.settings.yaml.schemas or {},
+              require("schemastore").yaml.schemas()
+            )
+          end,
+          settings = {
+            redhat = { telemetry = { enabled = false } },
+            yaml = {
+              keyOrdering = false,
+              format = {
+                enable = true,
+              },
+              validate = true,
+              schemaStore = {
+                -- Must disable built-in schemaStore support to use
+                -- schemas from SchemaStore.nvim plugin
+                enable = false,
+                -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+                url = "",
+              },
+            },
+          }
         })
       end,
       ['jsonls'] = function()
         local capabilities = require('cmp_nvim_lsp').default_capabilities()
         require('lspconfig').jsonls.setup({
           capabilities = capabilities,
+          on_new_config = function(new_config)
+            new_config.settings.json.schemas = new_config.settings.json.schemas or {}
+            vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
+          end,
           settings = {
             json = {
-              schemas = require('schemastore').json.schemas(),
+              format = {
+                enable = true,
+              },
               validate = { enable = true },
             },
           },
