bd00b6ee8f8281055be9e9c0d235925d010f6823
Kevin Durbin <kevindurb@fastmail.com>
2024-09-24 11:04:57 -0600

    fix: fix devicons mock

--- dot_config/nvim/lua/plugins/mini.icons.lua
+++ dot_config/nvim/lua/plugins/mini.icons.lua
@@ -1,7 +1,19 @@
 return {
   'echasnovski/mini.nvim',
-  setup = function()
-    require('mini.icons').setup()
-    require('mini.icons').mock_nvim_web_devicons()
+  lazy = true,
+  opts = {
+    file = {
+      ['.keep'] = { glyph = '󰊢', hl = 'MiniIconsGrey' },
+      ['devcontainer.json'] = { glyph = '', hl = 'MiniIconsAzure' },
+    },
+    filetype = {
+      dotenv = { glyph = '', hl = 'MiniIconsYellow' },
+    },
+  },
+  init = function()
+    package.preload['nvim-web-devicons'] = function()
+      require('mini.icons').mock_nvim_web_devicons()
+      return package.loaded['nvim-web-devicons']
+    end
   end,
 }
